e052c43e0db3f0420f99cf6f94a7764a7c34c184
AmeerOquendo <ameeroquendo29@gmail.com>
2026-03-02 23:17:33 +0800

    Generating qr code python object

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -2,3 +2,6 @@ import qrcode
 
 url = input("Enter the URL here: ").strip()
 file_path = "C:\\Users\\Ameer\\Downloads\\PUP\\Python\\qr_code_generator\\qr_code_generator.png"
+
+qr = qrcode.QRcode
+qr.add_data(url)
