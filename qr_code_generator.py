f8be58c01fe2232ac835dae40b8ff88556f528da
AmeerOquendo <ameeroquendo29@gmail.com>
2026-03-02 23:44:56 +0800

    I fix the wrong capitalization on QRCode and finished the project

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,7 +1,12 @@
 import qrcode
 
 url = input("Enter the URL here: ").strip()
-file_path = "C:\\Users\\Ameer\\Downloads\\PUP\\Python\\qr_code_generator\\qr_code_generator.png"
+file_path = "C:\\Users\\Ameer\\Downloads\\PUP\\Python\\qr_code_generator_ni_pogi\\qr_code_generator.png"
 
-qr = qrcode.QRcode
+qr = qrcode.QRCode()
 qr.add_data(url)
+
+img = qr.make_image()
+img.save(file_path)
+
+print("QR Code was generated")
